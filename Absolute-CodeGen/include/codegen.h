0af872d136211209a6be7e52aabff9b878b2a932
FuryBaM <51872689+FuryBaM@users.noreply.github.com>
2026-07-18 11:00:13 +0500

    Add LLVM IR backend: Absolute-CodeGen/include/codegen.h

---
+++ Absolute-CodeGen/include/codegen.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "expression_visitor.h"
+#include "statement_visitor.h"
+
+#include <memory>
+#include <string>
+
+namespace Absolute {
+    class CodeGenerator final : public ExpressionVisitor, public StatementVisitor {
+    public:
+        CodeGenerator();
+        ~CodeGenerator() override;
+
+        CodeGenerator(const CodeGenerator&) = delete;
+        CodeGenerator& operator=(const CodeGenerator&) = delete;
+
+        std::string Generate(Program& program, const std::string& moduleName);
+
+        void Visit(PrimitiveTypeExpr* expr) override;
+        void Visit(UserTypeExpr* expr) override;
+        void Visit(IdentifierExpr* expr) override;
+        void Visit(FunctionCallExpr* expr) override;
+        void Visit(ArrayAccessExpr* expr) override;
+        void Visit(BinaryExpr* expr) override;
+        void Visit(TernaryExpr* expr) override;
+        void Visit(NullExpr* expr) override;
+        void Visit(BooleanLiteralExpr* expr) override;
+        void Visit(NumberLiteralExpr* expr) override;
+        void Visit(StringLiteralExpr* expr) override;
+        void Visit(CharLiteralExpr* expr) override;
+        void Visit(ArrayExpr* expr) override;
+        void Visit(AssignmentExpr* expr) override;
+        void Visit(VarDeclExpr* expr) override;
+        void Visit(MemberAccessExpr* expr) override;
+        void Visit(CastExpr* expr) override;
+        void Visit(ConstructorCallExpr* expr) override;
+        void Visit(DestructorCallExpr* expr) override;
+        void Visit(InstanceDeclExpr* expr) override;
+        void Visit(PrefixUnaryExpr* expr) override;
+        void Visit(PostfixUnaryExpr* expr) override;
+        void Visit(TemplateExpr* expr) override;
+
+        void Visit(SingleStatement* stmt) override;
+        void Visit(CompoundStmt* stmt) override;
+        void Visit(FunctionCallStmt* stmt) override;
+        void Visit(FunctionDeclStmt* stmt) override;
+        void Visit(ReturnStmt* stmt) override;
+        void Visit(AssignmentStmt* stmt) override;
+        void Visit(VarDeclStmt* stmt) override;
+        void Visit(StructDeclStmt* stmt) override;
+        void Visit(ClassDeclStmt* stmt) override;
+        void Visit(ConstructorDeclStmt* stmt) override;
+        void Visit(EnumDeclStmt* stmt) override;
+        void Visit(GroupDeclStmt* stmt) override;
+        void Visit(IfStmt* stmt) override;
+        void Visit(ForStmt* stmt) override;
+        void Visit(WhileStmt* stmt) override;
+        void Visit(DoWhileStmt* stmt) override;
+        void Visit(ForEachStmt* stmt) override;
+        void Visit(ContinueStmt* stmt) override;
+        void Visit(BreakStmt* stmt) override;
+
+    private:
+        struct Impl;
+        std::unique_ptr<Impl> impl;
+    };
+}
