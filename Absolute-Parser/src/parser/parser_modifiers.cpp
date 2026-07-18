d202e92d6e324d8875c86a1aa5bbf1d269670457
FuryBaM <51872689+FuryBaM@users.noreply.github.com>
2026-07-18 09:10:08 +0500

    Stabilize compiler foundation: Absolute-Parser/src/parser/parser_modifiers.cpp

--- Absolute-Parser/src/parser/parser_modifiers.cpp
+++ Absolute-Parser/src/parser/parser_modifiers.cpp
@@ -5,7 +5,7 @@ namespace Absolute {
     void Parser::ParseModifiers() {
         modifiers.clear(); // Очищаем перед новым выражением
 
-        while (CurrentToken()->type == TokenType::KEYWORD && IsModifier(CurrentToken()->value)) {
+        while (CurrentToken() && CurrentToken()->type == TokenType::KEYWORD && IsModifier(CurrentToken()->value)) {
             modifiers.push_back(*Consume(TokenType::KEYWORD));
         }
     }
