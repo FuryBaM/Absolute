919b4fecc93c9a9c64bc65711c041b006e4f200d
FuryBaM <51872689+FuryBaM@users.noreply.github.com>
2026-07-18 09:10:38 +0500

    Stabilize compiler foundation: Absolute-Parser/src/parser/parser_arguments.cpp

--- Absolute-Parser/src/parser/parser_arguments.cpp
+++ Absolute-Parser/src/parser/parser_arguments.cpp
@@ -6,15 +6,20 @@ namespace Absolute{
     {
         Consume(TokenType::BRACKET, "("); // "("
         std::vector<std::unique_ptr<Expression>> arguments;
-        while (CurrentToken()->type != TokenType::BRACKET && CurrentToken()->value != ")")
+        while (!(RequireCurrent("an argument or ')'")->type == TokenType::BRACKET && CurrentToken()->value == ")"))
         {
             std::unique_ptr<Expression> argument = ParseExpression();
             arguments.push_back(std::move(argument));
-            if (CurrentToken()->type == TokenType::DELIMITER && CurrentToken()->value == ",") {
+            Token* current = RequireCurrent("',' or ')'");
+            if (current->type == TokenType::DELIMITER && current->value == ",") {
                 Consume(TokenType::DELIMITER, ",");
             }
             else if (CurrentToken()->type == TokenType::BRACKET && CurrentToken()->value == ")") {
                 break;
+            }
+            else {
+                ReportSyntaxError(CurrentToken(), "Expected ',' or ')' after argument");
+                throw std::runtime_error("Invalid argument list");
             }
         }
         Consume(TokenType::BRACKET, ")"); // ")"
