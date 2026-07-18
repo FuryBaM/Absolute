916383c36350fd77c17ab6f7b3aa113c9535b3ed
FuryBaM <51872689+FuryBaM@users.noreply.github.com>
2026-07-18 09:46:45 +0500

    Add statement visitor interface: Absolute-Parser/include/statement_visitor.h

---
+++ Absolute-Parser/include/statement_visitor.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "syntax_tree.h"
+
+namespace Absolute {
+    class StatementVisitor {
+    public:
+        virtual ~StatementVisitor() = default;
+
+        virtual void Visit(SingleStatement* stmt) = 0;
+        virtual void Visit(CompoundStmt* stmt) = 0;
+        virtual void Visit(FunctionCallStmt* stmt) = 0;
+        virtual void Visit(FunctionDeclStmt* stmt) = 0;
+        virtual void Visit(ReturnStmt* stmt) = 0;
+        virtual void Visit(AssignmentStmt* stmt) = 0;
+        virtual void Visit(VarDeclStmt* stmt) = 0;
+        virtual void Visit(StructDeclStmt* stmt) = 0;
+        virtual void Visit(ClassDeclStmt* stmt) = 0;
+        virtual void Visit(ConstructorDeclStmt* stmt) = 0;
+        virtual void Visit(EnumDeclStmt* stmt) = 0;
+        virtual void Visit(GroupDeclStmt* stmt) = 0;
+        virtual void Visit(IfStmt* stmt) = 0;
+        virtual void Visit(ForStmt* stmt) = 0;
+        virtual void Visit(WhileStmt* stmt) = 0;
+        virtual void Visit(DoWhileStmt* stmt) = 0;
+        virtual void Visit(ForEachStmt* stmt) = 0;
+        virtual void Visit(ContinueStmt* stmt) = 0;
+        virtual void Visit(BreakStmt* stmt) = 0;
+    };
+}
